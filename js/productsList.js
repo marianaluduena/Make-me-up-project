b2d10a8619c47e9376781ecf2090174bbc00e376
Mariana Luduena <marianabelenluduena@gmail.com>
2023-02-26 14:03:21 -0300

    Products added
    Products added to folder img and js

---
+++ js/productsList.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: 1,
+        name: "",
+        prize: 2.000,
+        img: "../img/products/crema__corporal.png",
+    },
+
+    {
+        id: 2,
+        name: "",
+        prize: 1.500,
+        img: "../img/products/delineador.png"
+
+    },
+
+    {
+        id: 3,
+        name: "",
+        prize: 2.500,
+        img: "../img/products/labial.png",
+    },
+
+    {
+        id: 4,
+        name: "",
+        prize: 1.000,
+        img: "../img/products/sombra__de__ojos.png",
+    },
+
+    {
+        id: 5,
+        name: "Shampoo",
+        prize: 3.000,
+        img:"../img/products/shampoo.png",
+    },
+
+    {
+        id: 6,
+        name: "Rouge",
+        prize: 3.500,
+        img: "../img/products/rubor.png"
+    }
+];
