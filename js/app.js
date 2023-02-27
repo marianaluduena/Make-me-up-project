62df0dd43b97e69db51315f8263fa65e1829720d
Mariana Luduena <marianabelenluduena@gmail.com>
2023-02-26 22:42:51 -0300

    Js products for each
    Now the products are added into the index dynamicly.

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const shopContent = document.getElementById("shopContent"); // div in the index with the ID shopContent
+
+let shoppingCart = [];
+
--- js/productsList.js
+++ js/productsList.js
@@ -1,44 +1,66 @@
-const products = [
-    {
+const products = [{
         id: 1,
-        name: "",
-        prize: 2.000,
+        name: "Body lotion",
+        price: 2.000,
         img: "../img/products/crema__corporal.png",
     },
 
     {
         id: 2,
-        name: "",
-        prize: 1.500,
+        name: "Black eyeliner",
+        price: 1.500,
         img: "../img/products/delineador.png"
 
     },
 
     {
         id: 3,
-        name: "",
-        prize: 2.500,
+        name: "Red Lipstick",
+        price: 2.500,
         img: "../img/products/labial.png",
     },
 
     {
         id: 4,
-        name: "",
-        prize: 1.000,
+        name: "Eye shadow palette",
+        price: 1.000,
         img: "../img/products/sombra__de__ojos.png",
     },
 
     {
         id: 5,
         name: "Shampoo",
-        prize: 3.000,
-        img:"../img/products/shampoo.png",
+        price: 3.000,
+        img: "../img/products/shampoo.png",
     },
 
     {
         id: 6,
-        name: "Rouge",
-        prize: 3.500,
+        name: "Pink Blush",
+        price: 3.500,
         img: "../img/products/rubor.png"
     }
 ];
+
+products.forEach((product) => {
+
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    
+    <img class="img__product" src="${product.img}" />
+    <h3>${product.name} </h3>
+    <p>${product.price} $</p>
+    `;
+
+    // Adding the array of products to the index inside a div called shopContent
+    shopContent.append(content);
+
+    // Creating a buy btn
+
+    let buyBtn = document.createElement("button");
+    buyBtn.className = "btn btn-dark buy__btn"
+    buyBtn.innerText = "Buy";
+
+    shopContent.append(buyBtn);
+});
