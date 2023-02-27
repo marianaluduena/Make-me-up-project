ef3f7893c12920d23363cff3c355e5d79513d3f6
Mariana Luduena <marianabelenluduena@gmail.com>
2023-02-27 15:16:00 -0300

    Btn append commented

--- js/productsList.js
+++ js/productsList.js
@@ -1,45 +1,45 @@
 const products = [{
-        id: 1,
-        name: "Body lotion",
-        price: 2.000,
-        img: "../img/products/crema__corporal.png",
-    },
+    id: 1,
+    name: "Body lotion",
+    price: 2.000,
+    img: "../img/products/crema__corporal.png",
+},
 
-    {
-        id: 2,
-        name: "Black eyeliner",
-        price: 1.500,
-        img: "../img/products/delineador.png"
+{
+    id: 2,
+    name: "Black eyeliner",
+    price: 1.500,
+    img: "../img/products/delineador.png"
 
-    },
+},
 
-    {
-        id: 3,
-        name: "Red Lipstick",
-        price: 2.500,
-        img: "../img/products/labial.png",
-    },
+{
+    id: 3,
+    name: "Red Lipstick",
+    price: 2.500,
+    img: "../img/products/labial.png",
+},
 
-    {
-        id: 4,
-        name: "Eye shadow palette",
-        price: 1.000,
-        img: "../img/products/sombra__de__ojos.png",
-    },
+{
+    id: 4,
+    name: "Eye shadow palette",
+    price: 1.000,
+    img: "../img/products/sombra__de__ojos.png",
+},
 
-    {
-        id: 5,
-        name: "Shampoo",
-        price: 3.000,
-        img: "../img/products/shampoo.png",
-    },
+{
+    id: 5,
+    name: "Shampoo",
+    price: 3.000,
+    img: "../img/products/shampoo.png",
+},
 
-    {
-        id: 6,
-        name: "Pink Blush",
-        price: 3.500,
-        img: "../img/products/rubor.png"
-    }
+{
+    id: 6,
+    name: "Pink Blush",
+    price: 3.500,
+    img: "../img/products/rubor.png"
+}
 ];
 
 products.forEach((product) => {
@@ -61,6 +61,6 @@ products.forEach((product) => {
     let buyBtn = document.createElement("button");
     buyBtn.className = "btn btn-dark buy__btn"
     buyBtn.innerText = "Buy";
-
+    //content.append(buyBtn);
     shopContent.append(buyBtn);
 });
