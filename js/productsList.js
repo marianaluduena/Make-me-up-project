a089ca1d5c8d59399d71b02e9eb4cfcd75d38719
Mariana Luduena <63405308+marianaluduena@users.noreply.github.com>
2023-08-13 09:24:37 -0300

    Banner + margin issue correction
    First banner added and  there was a margin issue on lg version now solved

--- js/productsList.js
+++ js/productsList.js
@@ -43,20 +43,20 @@ const products = [{
 ];
 
 products.forEach((product) => {
-
+ 
 
     let content = document.createElement("div");
-    content.className = "col-12 col-md-6 col-lg-4 m-2";
+    content.className = "col-12 col-md-6 col-lg-4 m-2 shop_card_product";
     content.innerHTML = `
-    <div class="row">
+    
     <div class="col-12 p-3">
     <img class="img__product img-fluid" src="${product.img}" alt="${product.name}" />
     <div class="card-body">
     <h3 class="text-center p-2 card-title">${product.name} </h3>
-    <p class="mx-1 card-text">${product.price} $</p>
+    <p class="d-flex justify-content-center mx-1 card-text">$ ${product.price} </p>
     </div>
     </div>
-    </div>
+    
     `;
 
     // Adding the array of products to the index inside a div called shopContent
