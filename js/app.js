b680b974538c9dbea1c559b87d412f18ece00c3f
Mariana Luduena <marianabelenluduena@gmail.com>
2023-08-23 14:34:05 -0300

    shopping cart js
    JS added to shopping cart to display

--- js/app.js
+++ js/app.js
@@ -59,19 +59,32 @@ products.forEach((product) => {
 
 showShoppingCart.addEventListener("click", () => {
 
+    // Clear all the items from the cart before showing the cart again and preventing to replicate the products with every click on the cart btn
+
+    modalContainer.innerHTML = " ";
+
+    // Code to show the cart every time I click the btn
+
+    modalContainer.style.display = "block";
 
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
-        <h5 class="modal-header-title p-2"> Your Cart<h5/>
+        <h2 class="modal-header-title p-2"> Your Cart<h2/>
         `
     modalContainer.append(modalHeader);
 
     // Close button modal
 
-    const modalCloseBtn = document.createElement("h1");
+    const modalCloseBtn = document.createElement("span");
     modalCloseBtn.innerText = "X";
-    modalCloseBtn.className = "modal-header-button";
+    modalCloseBtn.className = "modal-header-button m-3 p-2 d-flex";
+
+    modalCloseBtn.addEventListener("click", () => {
+
+        modalContainer.style.display = "none";
+
+    });
 
     modalHeader.append(modalCloseBtn);
 
@@ -84,10 +97,13 @@ showShoppingCart.addEventListener("click", () => {
         cartContent.innerHTML = `
 
     <div class="cart__products__container">
+   
     <img src = "${product.img}" alt= "${product.name}" class="img-fluid img__product__cart "/>
 
     <h3 class="p-2"> ${product.name} </h3>
     <p class="p-2"> $ ${product.price} </p>
+    <span class="d-flex justify-content-center"> X </span>
+
     <div>
     `
         modalContainer.append(cartContent);
